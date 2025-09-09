c3a4515aec4dd49392aaf7365ccf328f2b499f20
Wambita <wambitafana@gmail.com>
2025-09-10 00:35:22 +0300

    Feat: add date  of  birth  validation for  users  to always be  above  18 yrs

--- users/forms.py
+++ users/forms.py
@@ -4,6 +4,7 @@ from django.db import transaction
 from django.core.exceptions import ValidationError
 
 from .models import User, Company, Customer
+from django.db import transaction
 
 
 class DateInput(forms.DateInput):
@@ -15,6 +16,12 @@ def validate_email(value):
     if User.objects.filter(email=value).exists():
         raise ValidationError(
             value + " is already taken.")
+
+def validate_date_of_birth(value):
+    """Ensure  that date of birth is not beyond  dec 2009"""
+    max_date = date(2009, 12, 31)
+    if  value > max_date:
+        false = ValidationError("Date of birth cannot be beyond 31st Dec 2009.")
 
 
 class CustomerSignUpForm(UserCreationForm):
