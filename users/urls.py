7724ea4fbe461cf914eebcfe8beebefa46cd3869
Wambita <wambitafana@gmail.com>
2025-09-10 01:06:57 +0300

    Feat:  add url for  profile

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from django.contrib.auth import views
+from django.contrib.auth import views as auth_views
 
 from .forms import UserLoginForm
 from . import views as v
+app_name = "users"
 
 urlpatterns = [
     path('', v.register, name='register'),
     path('company/', v.CompanySignUpView.as_view(), name='register_company'),
     path('customer/', v.CustomerSignUpView.as_view(), name='register_customer'),
-    path('login/', v.LoginUserView, name='login_user')
+    path('login/', v.LoginUserView, name='login_user'),
+    path ('profile/', v.profile, name='profile'),
 ]
